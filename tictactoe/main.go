646329aab3fba9d273f2ee96af3ae57c04c359ae
Jasper <59110008+jasperschroeder@users.noreply.github.com>
2022-03-06 21:52:24 +0100

    Improved and finalized first version of tictactoe game.

--- tictactoe/main.go
+++ tictactoe/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
 	"time"
 )
-
-func Displayboard(board [9]string) {
-	fmt.Println(board[0], "|", board[1], "|", board[2])
-	fmt.Println(board[3], "|", board[4], "|", board[5])
-	fmt.Println(board[6], "|", board[7], "|", board[8])
-}
 
 func Displaywelcome() {
 	fmt.Println("----------------------------------------------------")
@@ -29,7 +21,21 @@ func Displaywelcome() {
 
 }
 
+func Displayboard(board [9]string) {
+	fmt.Println(board[0], "|", board[1], "|", board[2])
+	fmt.Println(board[3], "|", board[4], "|", board[5])
+	fmt.Println(board[6], "|", board[7], "|", board[8])
+}
+
 var gameOver bool = false
+var board = [...]string{"-", "-", "-", "-", "-", "-", "-", "-", "-"}
+
+func renewBoard(board [9]string) [9]string {
+	for a := 0; a < 9; a++ {
+		board[a] = "-"
+	}
+	return board
+}
 
 func Checkwin(board [9]string) {
 
@@ -50,26 +56,26 @@ func Checkwin(board [9]string) {
 		gameOver = true
 
 	} else if (board[1] == board[4] && board[4] == board[7]) && board[1] != "-" {
-		fmt.Println(board[7], "won!")
+		fmt.Println(board[1], "won!")
 		gameOver = true
 
 	} else if (board[2] == board[5] && board[5] == board[8]) && board[2] != "-" {
-		fmt.Println(board[6], "won!")
+		fmt.Println(board[2], "won!")
 		gameOver = true
 
 	} else if (board[0] == board[4] && board[4] == board[8]) && board[0] != "-" {
-		fmt.Println(board[6], "won!")
+		fmt.Println(board[0], "won!")
 		gameOver = true
 
 	} else if (board[2] == board[4] && board[4] == board[6]) && board[2] != "-" {
-		fmt.Println(board[6], "won!")
+		fmt.Println(board[2], "won!")
 		gameOver = true
 	}
 }
 
-func main() {
-	fmt.Println("Hello and welcome to a game of Tic Tac Toe!")
-	Displaywelcome()
+func play() {
+	gameOver = false
+	board = renewBoard(board)
 
 	rand.Seed(time.Now().UnixNano())
 
@@ -88,22 +94,12 @@ func main() {
 
 	fmt.Println(firstVal, "starts!")
 	fmt.Println(secondVal, "follows!")
-
-	board := [...]string{"-", "-", "-", "-", "-", "-", "-", "-", "-"}
 	Displayboard(board)
 
 	for a := 0; a < 9; a++ {
 
 		if !gameOver {
-
-			reader := bufio.NewReader(os.Stdin)
-			char, _, err := reader.ReadRune()
-
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			pos := int(char - 49) // unicode to integer
+			pos := inputPosition(board)
 
 			if a%2 == 0 {
 				board[pos] = firstVal
@@ -117,10 +113,47 @@ func main() {
 				Checkwin(board)
 			}
 		}
-
 	}
 
 	if !gameOver {
 		fmt.Println("It's a draw!")
+	}
+}
+
+func main() {
+	fmt.Println("Hello and welcome to a game of Tic Tac Toe!")
+	Displaywelcome()
+	play()
+
+	for {
+		fmt.Println("Fancy a new game? Input 'Y' if so.")
+		var yes string
+		fmt.Scan(&yes)
+
+		if yes == "Y" {
+			play()
+		} else {
+			fmt.Println("Bye bye!")
+			break
+		}
+	}
+
+}
+
+func inputPosition(board [9]string) int {
+
+	var position int
+	fmt.Scan(&position)
+
+	if position < 1 || position > 9 {
+		fmt.Println("Invalid input! Only allowed inputs are numbers in the range 1-9!")
+		return inputPosition(board)
+	}
+
+	if board[position-1] == "-" {
+		return position - 1
+	} else {
+		fmt.Println("Position", position, "is already taken. Try again!")
+		return inputPosition(board)
 	}
 }
